467b698e420520763dccfb02b15026ba194dad96
Christian Emmer <10749361+emmercm@users.noreply.github.com>
2024-03-17 19:35:04 -0700

    Feature: create & extract raw

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import ChdmanHd from './src/chdman/chdmanHd.js';
 import ChdmanCd from './src/chdman/chdmanCd.js';
 import ChdmanVerify from './src/chdman/chdmanVerify.js';
 import ChdmanDvd from './src/chdman/chdmanDvd.js';
+import ChdmanRaw from './src/chdman/chdmanRaw.js';
 
 export * from './src/chdman/chdmanInfo.js';
 export * from './src/chdman/chdmanBin.js';
@@ -11,6 +12,7 @@ export * from './src/chdman/chdmanHd.js';
 export * from './src/chdman/chdmanCd.js';
 export * from './src/chdman/chdmanVerify.js';
 export * from './src/chdman/chdmanDvd.js';
+export * from './src/chdman/chdmanRaw.js';
 export * from './src/chdman/common.js';
 
 export default {
@@ -20,13 +22,13 @@ export default {
 
   verify: ChdmanVerify.verify,
 
-  // TODO(cemmer): createraw
+  createRaw: ChdmanRaw.createRaw,
   createHd: ChdmanHd.createHd,
   createCd: ChdmanCd.createCd,
   createDvd: ChdmanDvd.createDvd,
   // TODO(cemmer): createld
 
-  // TODO(cemmer): extractraw
+  extractRaw: ChdmanRaw.extractRaw,
   extractHd: ChdmanHd.extractHd,
   extractCd: ChdmanCd.extractCd,
   extractDvd: ChdmanDvd.extractDvd,
