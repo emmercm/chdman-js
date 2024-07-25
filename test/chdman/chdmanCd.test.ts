956dd7a1032116b457df4a2b9f9859a663c3fa0a
Christian Emmer <10749361+emmercm@users.noreply.github.com>
2024-07-24 17:22:54 -0700

    Fix: create CD test hunk size

--- test/chdman/chdmanCd.test.ts
+++ test/chdman/chdmanCd.test.ts
@@ -31,9 +31,9 @@ it('should fail on nonexistent file', async () => {
 });
 
 test.each([
-  [path.join('test', 'fixtures', 'cue', 'single.cue'), 2352],
-  [path.join('test', 'fixtures', 'cue', 'multiple.cue'), Math.floor((4704 + 7056 + 3000) / 2352) * 2352],
-])('should create, info, and extract: %s', async (cue, expectedBinSize) => {
+  [path.join('test', 'fixtures', 'cue', 'single.cue'), 19_584, 2352],
+  [path.join('test', 'fixtures', 'cue', 'multiple.cue'), 14_688, Math.floor((4704 + 7056 + 3000) / 2352) * 2352],
+])('should create, info, and extract: %s', async (cue, hunkSize, expectedBinSize) => {
   const temporaryChd = `${await TestUtil.mktemp(path.join(os.tmpdir(), path.basename(cue)))}.chd`;
   const temporaryCue = `${temporaryChd}.cue`;
   const temporaryBin = `${temporaryChd}.bin`;
@@ -43,7 +43,7 @@ test.each([
       inputFilename: cue,
       outputFilename: temporaryChd,
       // chdman v0.263 stopped producing valid CHDs for the fixtures with default settings
-      hunkSize: 2448 * 6,
+      hunkSize,
     });
     await expect(TestUtil.exists(temporaryChd)).resolves.toEqual(true);
 
