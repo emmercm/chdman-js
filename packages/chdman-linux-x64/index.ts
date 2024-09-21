cfc826beb0ca007e3b593a2992e9b8c9855d1f2a
Christian Emmer <10749361+emmercm@users.noreply.github.com>
2024-09-20 21:25:32 -0700

    CI: don't look for binaries using fs & walk

--- packages/chdman-linux-x64/index.ts
+++ packages/chdman-linux-x64/index.ts
@@ -1,25 +1,4 @@
-import fs from 'node:fs';
 import path from 'node:path';
 import url from 'node:url';
 
-/**
- * Search for a {@link fileName} in {@link filePath} or any of its parent directories.
- */
-function scanUpPathForFile(filePath: string, fileName: string): string | undefined {
-  const fullPath = path.join(filePath, fileName);
-  if (fs.existsSync(fullPath)) {
-    return fullPath;
-  }
-
-  const parentPath = path.dirname(filePath);
-  if (parentPath !== filePath) {
-    return scanUpPathForFile(path.dirname(filePath), fileName);
-  }
-
-  return undefined;
-}
-
-export default scanUpPathForFile(
-  url.fileURLToPath(new URL('.', import.meta.url)),
-  'chdman',
-);
+export default path.join(url.fileURLToPath(new URL('.', import.meta.url)), 'chdman');
