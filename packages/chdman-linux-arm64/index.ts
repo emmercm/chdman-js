a14a4bae35ec39e72bdefcf392970d7123913b53
Christian Emmer <10749361+emmercm@users.noreply.github.com>
2024-09-20 20:58:54 -0700

    CI: fix not finding the binary in another directory

--- packages/chdman-linux-arm64/index.ts
+++ packages/chdman-linux-arm64/index.ts
@@ -1,4 +1,25 @@
+import fs from 'node:fs';
 import path from 'node:path';
 import url from 'node:url';
 
-export default path.join(url.fileURLToPath(new URL('.', import.meta.url)), 'chdman');
+/**
+ * Search for a {@link fileName} in {@link filePath} or any of its parent directories.
+ */
+function scanUpPathForFile(filePath: string, fileName: string): string | undefined {
+  const fullPath = path.join(filePath, fileName);
+  if (fs.existsSync(fullPath)) {
+    return fullPath;
+  }
+
+  const parentPath = path.dirname(filePath);
+  if (parentPath !== filePath) {
+    return scanUpPathForFile(path.dirname(filePath), fileName);
+  }
+
+  return undefined;
+}
+
+export default scanUpPathForFile(
+  url.fileURLToPath(new URL('.', import.meta.url)),
+  'chdman',
+);
