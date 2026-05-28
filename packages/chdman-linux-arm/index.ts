e57fd0e3c78c283a2eb62026156b734ad979aba9
Christian Emmer <10749361+emmercm@users.noreply.github.com>
2026-05-28 19:52:47 -0400

    ESLint

--- packages/chdman-linux-arm/index.ts
+++ packages/chdman-linux-arm/index.ts
@@ -2,4 +2,4 @@ import path from 'node:path';
 import url from 'node:url';
 
 const armVersion = (process.config.variables as { arm_version?: number }).arm_version ?? '7';
-export default path.join(url.fileURLToPath(new URL('.', import.meta.url)), 'chdman-armv' + armVersion);
+export default path.join(url.fileURLToPath(new URL('.', import.meta.url)), `chdman-armv${armVersion}`);
